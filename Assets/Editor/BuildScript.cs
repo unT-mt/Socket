34ff076f1254ec7bb8d79e5ac4e1bd36ebeb4d7b
unT-mt <m-takashima@un-t.com>
2024-10-10 11:29:49 +0900

    各サンプルシーンをBuildをMenuから実行できるようにする（マニュアルの設定を毎回する作業を回避するため）

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -4,6 +4,8 @@ using System.IO;
 public class BuildScript
 {
     static string[] scenes = { "Assets/Scenes/Client.unity", "Assets/Scenes/Server.unity" };
+    static string[] urgSenderSampleScene = {"Assets/Scenes/URGSenderSample.unity"};
+    static string[] urgReceiverSampleScene = {"Assets/Scenes/URGReceiverSample.unity"};
 
     [MenuItem("Custom Build/Build Client and Server")]
     public static void BuildClientAndServer()
@@ -17,5 +19,37 @@ public class BuildScript
 
         // Serverのビルド
         BuildPipeline.BuildPlayer(new[] { scenes[1] }, serverPath, BuildTarget.StandaloneWindows64, BuildOptions.None);
+    }
+    
+    // 1. Build URGSenderSample メニューアイテム
+    [MenuItem("Custom Build/Build URGSenderSample")]
+    public static void BuildURGSenderSample()
+    {
+        // 解像度とウィンドウサイズの設定
+        PlayerSettings.defaultScreenWidth = 1920;
+        PlayerSettings.defaultScreenHeight = 1080;
+        PlayerSettings.resizableWindow = true;
+
+        // ビルドパス
+        string senderPath = @"C:\Users\m-takashima\UnityBuild\Socket\URGSenderSample\URGSenderSample.exe";
+        
+        // ビルド実行
+        BuildPipeline.BuildPlayer(new[] { urgSenderSampleScene[0] }, senderPath, BuildTarget.StandaloneWindows64, BuildOptions.None);
+    }
+
+    // 2. Build URGReceiverSample メニューアイテム
+    [MenuItem("Custom Build/Build URGReceiverSample")]
+    public static void BuildURGReceiverSample()
+    {
+        // 解像度設定（後で変えられる想定）
+        PlayerSettings.defaultScreenWidth = 1920;
+        PlayerSettings.defaultScreenHeight = 1080;
+        PlayerSettings.resizableWindow = false; // resizableWindowをオフにする
+
+        // ビルドパス
+        string receiverPath = @"C:\Users\m-takashima\UnityBuild\Socket\URGReceiverSample\URGReceiverSample.exe";
+
+        // ビルド実行
+        BuildPipeline.BuildPlayer(new[] { urgReceiverSampleScene[0] }, receiverPath, BuildTarget.StandaloneWindows64, BuildOptions.None);
     }
 }

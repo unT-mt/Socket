13d8dd65dfce2d3d5ed25948e74a3478679331f9
unT-mt <m-takashima@un-t.com>
2024-10-18 10:43:27 +0900

    センサー送信側のwindowをつねに最前面にしないようにする ※つねに最背面になることはないが最前面のアプリを追い越して再起動することはないことを確認済

---
+++ Assets/Scripts/WindowControl.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System;
+using System.Runtime.InteropServices;
+
+public class WindowControl : MonoBehaviour
+{
+    [DllImport("user32.dll", SetLastError = true)]
+    static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+    [DllImport("user32.dll")]
+    static extern bool IsWindowVisible(IntPtr hWnd);
+
+    private static readonly IntPtr HWND_BOTTOM = new IntPtr(1); 
+    private const uint SWP_NOSIZE = 0x0001;
+    private const uint SWP_NOMOVE = 0x0002;
+    private const uint SWP_NOACTIVATE = 0x0010;
+
+    private IntPtr unityWindow;
+    private bool isWindowAtBack = false; // ウィンドウがバックにあるかどうか
+
+    void Start()
+    {
+        unityWindow = FindWindow(null, Application.productName);
+        if (unityWindow != IntPtr.Zero)
+        {
+            SetWindowToBackground();
+        }
+    }
+
+    void Update()
+    {
+        // ウィンドウがフォアグラウンドに出た場合のみ、バックグラウンドに戻す
+        if (unityWindow != IntPtr.Zero && !isWindowAtBack)
+        {
+            SetWindowToBackground();
+        }
+    }
+
+    private void SetWindowToBackground()
+    {
+        // フォアグラウンドにいるかどうかを確認し、必要な場合のみ実行
+        SetWindowPos(unityWindow, HWND_BOTTOM, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);
+        isWindowAtBack = true; // 一度バックに移動させたら、次の確認まで再度実行しない
+    }
+}
